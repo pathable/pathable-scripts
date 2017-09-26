3b054f199d20ccffb1ab9ce096173947e2ae3932
Faisal Ahmad <faisal.idreesi@gmail.com>
2017-09-26 19:53:12 +0500

    Needed to run git remote update before checking status for changes.

--- src/utilities/git/git-is-updated.js
+++ src/utilities/git/git-is-updated.js
@@ -1,11 +1,13 @@
 import { execSync } from 'child_process';
 
 export default function gitIsUpdated(localPath) {
-  const command = 'git status s';
+  let command = 'git remote update';
   const options = {
     cwd: localPath,
   };
+  execSync(command, options);
 
+  command = 'git status s';
   const buffer = execSync(command, options);
   const index = buffer.toString().indexOf('Your branch is up-to-date with');
   return index !== -1;
