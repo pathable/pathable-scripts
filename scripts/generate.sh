32204ba8d25a1002d78bed99e208de3b541a93a8
Nacho Codoñer <igcogi@gmail.com>
2017-11-17 14:29:48 +0100

    add widget generator script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,4 +6,9 @@
 load_env "$HOME/.pathable-env"
 load_env "config/$environment/.env"
 
-node ./node_modules/pathable-scripts/dist/generators/migrations/generate "$@"
+which=$1
+if [ "${which}" = "migration" ]; then
+  node ./node_modules/pathable-scripts/dist/generators/migrations/generate migration "$2"
+elif [ "${which}" = "widget" ]; then
+  ./node_modules/.bin/plop "$@"
+fi
